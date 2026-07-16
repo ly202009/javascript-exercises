cb0cfcf726cf7cf760e66531fdb312ca469b386b
ly202009 <leeyayoh@gmail.com>
2026-07-15 19:43:14 -0700

    Foundations 16 Complete

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    return arr.reduce((oldest, current) => {
+        // Get year of death, if yearOfDeath key not found then get present year
+        const died = Object.keys(current).includes("yearOfDeath") ? current.yearOfDeath : (new Date()).getFullYear();
+
+        if(died - current.yearOfBirth >= oldest.age){
+            return {name: current.name, age: died - current.yearOfBirth};
+        }else{
+            return oldest;
+        }
+    }, {name: "", age: 0})
 };
 
 // Do not edit below this line
