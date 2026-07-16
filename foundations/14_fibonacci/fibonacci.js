127c20cab3e3626d09ee9640687358898e34d1ee
ly202009 <leeyayoh@gmail.com>
2026-07-15 17:44:38 -0700

    Foundations 14 Complete

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if(n < 0){
+        return "OOPS";
+    }
+    let previous = 1;
+    let total = 0;
+    for(let i = 0; i < n; i++){
+        total += previous;
+        previous = Math.abs(total - previous);
+    }
+    return total;
 };
 
 // Do not edit below this line
