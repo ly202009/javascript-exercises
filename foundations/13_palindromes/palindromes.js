454b73fe62214158913882328e21416ab39bc8ba
ly202009 <leeyayoh@gmail.com>
2026-07-15 17:09:04 -0700

    Foundations 13 Complete

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanString = str.replace(/[^\w]/g, "").toLowerCase(); // Use RegEx to get non character/numerical chars
+    for(let i = 0; i < cleanString.length/2; i++){
+        if(cleanString[i] !== cleanString[cleanString.length-i-1]) return false;
+    }
+    return true;
+    
 };
 
 // Do not edit below this line
